54c2e713d13f0f8a474cdd0af200422d934b15fb
Oleg Smolovyk <oleg.smolovyk@mingus-software.com>
2018-11-03 15:54:41 -0400

    Added routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
+
 import './App.css';
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {MemoryRouter} from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
