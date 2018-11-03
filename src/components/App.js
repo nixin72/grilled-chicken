160082a14abf4acc4d780f49654f086cc65844b0
Oleg Smolovyk <oleg.smolovyk@mingus-software.com>
2018-11-03 15:39:56 -0400

    Added routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Home from "./Pages/Home/Home";
+import PostAnimal from "./Pages/PostAnimal/PostAnimal";
+import SearchAnimal from "./Pages/SearchAnimal/SearchAnimal";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        <Home/>
-        <Footer/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <Switch>
+                    <Route
+                        exact path="/"
+                        render={(props) => <Home
+                            {...props}
+                        />}
+                    />
+                    <Route
+                        exact path="/post"
+                        render={(props) => <PostAnimal
+                            {...props}
+                        />}
+                    />
+                    <Route
+                        exact path="/search"
+                        render={(props) => <SearchAnimal
+                            {...props}
+                        />}
+                    />
+                </Switch>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Pages/SearchAnimal/SearchAnimal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './SearchAnimal.css';
+
+class SearchAnimal extends Component {
+  render() {
+    return (
+      <div className="SearchAnimal">
+        SearchAnimal
+      </div>
+    );
+  }
+}
+
+export default SearchAnimal;
