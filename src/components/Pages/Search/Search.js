6db6166a3f1838c22f27e7845da5ed4517ce08d8
Philip Dumaresq <phdumaresq@gmail.com>
2018-11-04 08:53:32 -0500

    day 2

--- src/components/Pages/Search/Search.js
+++ src/components/Pages/Search/Search.js
@@ -8,7 +8,7 @@ export class Card extends Component {
                 <section id="petImage">
                     <img 
                         src="https://pbs.twimg.com/profile_images/962170088941019136/lgpCD8X4_400x400.jpg" 
-                        alt="Pet Image"/>
+                        alt="Pet"/>
                 </section>
 
                 <section id="petDescription">
@@ -49,100 +49,99 @@ class Search extends Component {
 
     render() {
         return (
-            <section>
-                <button className="dropDownButton" onClick={this.showHideFacets}>
-                    <hr width="1" size="10" />
-                    <hr width="1" size="10" />
-                    <hr width="1" size="10" />
-                </button>
-                
-                    
-                <section id="searchForm">
-                    <form>
-                        <div className="facet">
-                            <div>
-                                <label>Breed: </label>
-                            </div>
-                            <div>
-                                <input type="text" name="breed" />
-                            </div>
-                        </div>
-                        
-                        <br />
-
-                        <div className="form_section">
-                            <span>Size</span><br />
-                            <div className="facet" >
-                                <div>
-                                    <label>Small: </label><br />
-                                    <label>Medium: </label><br />
-                                    <label>Large: </label><br />
-                                </div>
-                                <div>
-                                    <input type="checkbox" name="size_s" /><br />
-                                    <input type="checkbox" name="size_m" /><br />
-                                    <input type="checkbox" name="size_l" /><br />
-                                </div>    
-                            </div>
-                        </div>
-                        
-                        <br />
-
-                        <div className="facet">
-                            <div>
-                                <label>Minimum age: </label>
-                                <label>Maximum age: </label>
-                            </div>
-                            <div>
-                                <input type="number" name="min_age" min="0" max="30"/><br />
-                                <input type="number" name="max_age" min="0" max="30"/>
-                            </div>
-                        </div>
-                        
-                        <br />
-
-                        <div className="form_section">
-                            <span>Sex</span><br />
+            <section id="searchPage" className="flex">
+                <div>
+                    <button className="dropDownButton" onClick={this.showHideFacets}>
+                        Filter
+                    </button>
+                                            
+                    <section id="searchForm">
+                        <form>
                             <div className="facet">
                                 <div>
-                                    <label>Male: </label><br />
-                                    <label>Female: </label>
+                                    <label>Breed: </label>
                                 </div>
                                 <div>
-                                    <input type="checkbox" name="sex_m" value="Male" /><br />
-                                    <input type="checkbox" name="sex_f" value="Female"  />
+                                    <input type="text" name="breed" />
                                 </div>
                             </div>
-                        </div>
-                        
-                        <br />
+                            
+                            <br />
 
-                        <div className="facet">
-                            <div>
-                                <label>Spayed/Neutered: </label>
+                            <div className="form_section">
+                                <span>Size</span><br />
+                                <div className="facet" >
+                                    <div>
+                                        <label>Small: </label><br />
+                                        <label>Medium: </label><br />
+                                        <label>Large: </label><br />
+                                    </div>
+                                    <div>
+                                        <input type="checkbox" name="size_s" /><br />
+                                        <input type="checkbox" name="size_m" /><br />
+                                        <input type="checkbox" name="size_l" /><br />
+                                    </div>    
+                                </div>
                             </div>
-                            <div>
-                                <input type="checkbox" name="neutered" />
-                            </div>
-                        </div>
+                            
+                            <br />
 
-                        <br />
+                            <div className="facet">
+                                <div>
+                                    <label>Minimum age: </label><br />
+                                    <label>Maximum age: </label>
+                                </div>
+                                <div>
+                                    <input type="number" name="min_age" min="0" max="30"/><br />
+                                    <input type="number" name="max_age" min="0" max="30"/>
+                                </div>
+                            </div>
+                            
+                            <br />
 
-                        <div className="facet">
-                            <div>
-                                <label>hypoallergenic: </label>
+                            <div className="form_section">
+                                <span>Sex</span><br />
+                                <div className="facet">
+                                    <div>
+                                        <label>Male: </label><br />
+                                        <label>Female: </label>
+                                    </div>
+                                    <div>
+                                        <input type="checkbox" name="sex_m" value="Male" /><br />
+                                        <input type="checkbox" name="sex_f" value="Female"  />
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <input type="checkbox" name="hypoallergenic" value="Hypo-Allergenic" />
+                            
+                            <br />
+
+                            <div className="facet">
+                                <div>
+                                    <label>Spayed/Neutered: </label>
+                                </div>
+                                <div>
+                                    <input type="checkbox" name="neutered" />
+                                </div>
                             </div>
-                        </div>
-                    </form>
-                </section>   
-                          
-                <div class="flex">
-                    <img src={'/image/arrowprev.png'} height="50"/>
+
+                            <br />
+
+                            <div className="facet">
+                                <div>
+                                    <label>hypoallergenic: </label>
+                                </div>
+                                <div>
+                                    <input type="checkbox" name="hypoallergenic" value="Hypo-Allergenic" />
+                                </div>
+                            </div>
+                        </form>
+                    </section>     
+                </div>
+
+                <div id="pet_display" className="flex">
+                    <img src={'/image/arrowprev.png'} height="50" onClick={this.getPrevPet} alt="Previous Pet" />
                     <Card />
-                    <img src={'./image/arrownext.png'} height="50"/>
+                    <img src={'/image/arrownext.png'} height="50" onClick={this.getNextPet} alt="Next Pet" />
                 </div>
                 
             </section>
