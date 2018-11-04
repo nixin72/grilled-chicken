c3d9811b21d8cbfdb368002ba7dabd09dda134f0
Oleg Smolovyk <oleg.smolovyk@mingus-software.com>
2018-11-04 15:47:56 -0500

    Seatch and petCard

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -4,9 +4,30 @@ import './Home.css';
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        Home
-      </div>
+        <div className="Home jumbotron container-home">
+            <div className="container">
+                <div className="row">
+                    <div className="col-6 col-sm-6">
+                        <div className="jumbotron placeHome">
+                            <div className="container text-right">
+                                <p className="text-center">Dogs looking for a new home?</p>
+                                <button className="btn btn-outline-alert my-2 my-sm-0 " type="submit"><h1
+                                    className="text-center bold-text">Place</h1></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 col-sm-6">
+                        <div className="jumbotron findDog">
+                            <div className="container text-right">
+                                <p className="text-center">Are you looking to adopt a dog?</p>
+                                <button className="btn btn-outline-alert my-2 my-sm-0 " type="submit"><h1
+                                    className="text-center bold-text">Adopt</h1></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
   }
 }
--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Login.css';
-import DbContext from './../../../store/db-context';
+import DbContext from './../../../store/db-context'
 
 class Login extends Component {
 
