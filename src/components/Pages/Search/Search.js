f4f5a515e31ef1c4adbacc64ff2d9230331b02ae
Oleg Smolovyk <oleg.smolovyk@mingus-software.com>
2018-11-04 13:21:45 -0500

    Seatch and petCard

--- src/components/Pages/Search/Search.js
+++ src/components/Pages/Search/Search.js
@@ -1,76 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Search.css';
 import pets from "./pets.js";
+import DbContext from './../../../store/db-context';
 
-class Card extends Component {
-    hideEmptyFields(e) {
-        console.log("target: ", e.target);
-    }
+import PetCard from './PetCard/PetCard';
 
-    render() {
-        return (
-            <section id="pet_card">
-                <section id="petImage">
-                    <img 
-                        src={this.props.pet.img} 
-                        alt="Pet"/>
-                </section>
-
-                <section id="petDescription" onLoad={this.hideEmptyFields}>
-                    <div>
-                        <div className="petData">
-                            <label>Name: </label>
-                            <span id="name">{this.props.pet.name}</span>
-                        </div>
-                        <div className="petData">
-                            <span id="desc1">{this.props.pet.desc1}</span>
-                        </div>
-                        <div className="petData">
-                            <label>Personality: </label>
-                            <span id="personality">{this.props.pet.personality.join(",")}</span>
-                        </div>
-                        <div className="petData">
-                            <label>Height: </label>
-                            <span id="height">{this.props.pet.height}</span>
-                        </div>
-                        <div className="petData">
-                            <label>Weight: </label>
-                            <span id="weight">{this.props.pet.weight}</span>
-                        </div>
-                        <div className="petData">
-                            <label>Age: </label>
-                            <span id="age">{this.props.pet.age}</span>
-                        </div>
-                        <div className="petData">
-                            <label>Neutered: </label>
-                            <span id="neutered">{this.props.pet.neutered}</span>
-                        </div>
-                        <div className="petData">
-                            <label>Hypoallergenic: </label>
-                            <span id="hypoallergenic">{this.props.pet.hypoallergenic}</span>
-                        </div>
-                        <div className="petData">
-                            <span id="desc2">{this.props.pet.desc2}</span>
-                        </div>
-                    </div>
-                </section>
-            </section>
-        );
-    }
-}
 
 class Search extends Component {
+
+    static contextType = DbContext;
+
     constructor(props) {
         super(props);
 
-        this.currentIndex = 0;
-        this.currentPet = pets[this.currentIndex];
+        this.state = {
+            currentPet: undefined,
+            currentIndex: 0,
+        };
 
-        this.petComponent = <Card pet={this.currentPet} />;
+
 
         this.showHideFacets = this.showHideFacets.bind(this);
         this.getNextPet = this.getNextPet.bind(this);
         this.getPrevPet = this.getPrevPet.bind(this);
+    }
+
+    componentDidMount(){
+        this.setState({
+            currentPet: this.context.animalPropositionsDb[this.state.currentIndex],
+        });
     }
 
     showHideFacets() {
@@ -83,31 +41,23 @@ class Search extends Component {
     }
 
     getNextPet() {
-        console.log(this.currentPet);
-        if (this.currentIndex == pets.length-1) {
-            this.currentIndex = 0;
-        }
-        else {
-            this.currentIndex++;
-        }
-
-        this.currentPet = pets[this.currentIndex];
-        this.petComponent = <Card pet={this.currentPet} />;
-        this.render();
+        const {currentIndex} = this.state;
+        const pets = this.context.animalPropositionsDb;
+        let nextIndex = pets.length > currentIndex ? currentIndex + 1 : currentIndex;
+        this.setState({
+            currentPet: pets[nextIndex],
+            currentIndex: nextIndex,
+        });
     }
 
     getPrevPet() {
-        console.log(this.currentPet);
-        if (this.currentIndex == 0) {
-            this.currentIndex = pets.length-1;
-        }
-        else {
-            this.currentIndex--;
-        }
-
-        this.currentPet = pets[this.currentIndex];
-        this.petComponent = <Card pet={this.currentPet} />;
-        this.render();
+        const {currentIndex} = this.state;
+        const pets = this.context.animalPropositionsDb;
+        let prevIndex = currentIndex !== 0 ? currentIndex - 1 : currentIndex;
+        this.setState({
+            currentPet: pets[prevIndex],
+            currentIndex: prevIndex,
+        });
     }
 
     render() {
@@ -117,7 +67,7 @@ class Search extends Component {
                     <button className="dropDownButton" onClick={this.showHideFacets}>
                         Filter
                     </button>
-                                            
+
                     <section id="searchForm">
                         <form>
                             <div className="facet">
@@ -125,96 +75,101 @@ class Search extends Component {
                                     <label>Breed: </label>
                                 </div>
                                 <div>
-                                    <input type="text" name="breed" />
+                                    <input type="text" name="breed"/>
                                 </div>
                             </div>
-                            
-                            <br />
+
+                            <br/>
 
                             <div className="form_section">
-                                <span>Size</span><br />
-                                <div className="facet" >
+                                <span>Size</span><br/>
+                                <div className="facet">
                                     <div>
-                                        <label>Small: </label><br />
-                                        <label>Medium: </label><br />
-                                        <label>Large: </label><br />
+                                        <label>Small: </label><br/>
+                                        <label>Medium: </label><br/>
+                                        <label>Large: </label><br/>
                                     </div>
                                     <div>
-                                        <input type="checkbox" name="size_s" /><br />
-                                        <input type="checkbox" name="size_m" /><br />
-                                        <input type="checkbox" name="size_l" /><br />
-                                    </div>    
+                                        <input type="checkbox" name="size_s"/><br/>
+                                        <input type="checkbox" name="size_m"/><br/>
+                                        <input type="checkbox" name="size_l"/><br/>
+                                    </div>
                                 </div>
                             </div>
-                            
-                            <br />
+
+                            <br/>
 
                             <div className="facet">
                                 <div>
-                                    <label>Minimum age: </label><br />
+                                    <label>Minimum age: </label><br/>
                                     <label>Maximum age: </label>
                                 </div>
                                 <div>
-                                    <input type="number" name="min_age" min="0" max="30"/><br />
+                                    <input type="number" name="min_age" min="0" max="30"/><br/>
                                     <input type="number" name="max_age" min="0" max="30"/>
                                 </div>
                             </div>
-                            
-                            <br />
+
+                            <br/>
 
                             <div className="form_section">
-                                <span>Sex</span><br />
+                                <span>Sex</span><br/>
                                 <div className="facet">
                                     <div>
-                                        <label>Male: </label><br />
+                                        <label>Male: </label><br/>
                                         <label>Female: </label>
                                     </div>
                                     <div>
-                                        <input type="checkbox" name="sex_m" value="Male" /><br />
-                                        <input type="checkbox" name="sex_f" value="Female"  />
+                                        <input type="checkbox" name="sex_m" value="Male"/><br/>
+                                        <input type="checkbox" name="sex_f" value="Female"/>
                                     </div>
                                 </div>
                             </div>
-                            
-                            <br />
+
+                            <br/>
 
                             <div className="facet">
                                 <div>
                                     <label>Spayed/Neutered: </label>
                                 </div>
                                 <div>
-                                    <input type="checkbox" name="neutered" />
+                                    <input type="checkbox" name="neutered"/>
                                 </div>
                             </div>
 
-                            <br />
+                            <br/>
 
                             <div className="facet">
                                 <div>
                                     <label>hypoallergenic:</label>
                                 </div>
                                 <div>
-                                    <input type="checkbox" name="hypoallergenic" value="Hypo-Allergenic" />
+                                    <input type="checkbox" name="hypoallergenic" value="Hypo-Allergenic"/>
                                 </div>
                             </div>
                         </form>
-                    </section>   
+                    </section>
                     <section>
-                        
-                    </section>  
+
+                    </section>
                 </div>
 
                 <div id="pet_display" className="flex">
-                    <img src={'/image/arrowprev.png'} height="50" onClick={this.getPrevPet} alt="Previous Pet" />
+                    <img src={'/image/arrowprev.png'} height="50" onClick={this.getPrevPet} alt="Previous Pet"/>
                     <div>
-                        {this.petComponent}
-                        <br />
+                        { this.state.currentPet
+                                ?
+                                <PetCard pet={this.state.currentPet}/>
+                                :
+                                ''
+                        }
+                        <br/>
                         <div className="flex" id="vote">
                             <img src={'/image/upvote.png'} alt="Like button" height="50"/>
-                            <img src={'/image/downvote.png'} alt="Dislike button" height="50" />
+                            <img src={'/image/downvote.png'} alt="Dislike button" height="50"/>
                         </div>
                     </div>
-                    <img src={'/image/arrownext.png'} height="50" onClick={this.getNextPet} alt="Next Pet" />
+                    <img src={'/image/arrownext.png'} height="50" onClick={this.getNextPet} alt="Next Pet"/>
                 </div>
             </section>
         );
