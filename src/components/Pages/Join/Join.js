bd67f46e3c8e87d2b5224d004968785a017edcef
Philip Dumaresq <phdumaresq@gmail.com>
2018-11-04 17:41:38 -0500

    style changes

--- src/components/Pages/Join/Join.js
+++ src/components/Pages/Join/Join.js
@@ -4,21 +4,25 @@ import './Join.css';
 class Join extends Component {
     render() {
         return (
-          <form name="signup" method="POST" netlify>
-            <p>
-              <input type="text" name="name" placeholder="Full name"/>
-            </p>
-            <p>
-              <input type="text" name="email" placeholder="Email"/>
-            </p>
-            <p>
-              <input type="password" name="password" placeholder="password"></input>
-            </p>
-              <div data-netlify-recaptcha></div>
-            <p>
-              <input className="send" type="submit" value="Submit"/>
-            </p>
-          </form>
+          <div>
+            <h1>Register here!</h1>
+            <form name="signup" method="POST" netlify>
+              <p>
+                <input type="text" name="name" placeholder="Full name"/>
+              </p>
+              <p>
+                <input type="text" name="email" placeholder="Email"/>
+              </p>
+              <p>
+                <input type="password" name="password" placeholder="password"></input>
+              </p>
+                <div data-netlify-recaptcha></div>
+              <p>
+                <input className="send" type="submit" value="Submit"/>
+              </p>
+            </form>  
+          </div>
+          
         );
     }
 }
